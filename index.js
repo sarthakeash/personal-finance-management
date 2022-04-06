7684959e5266183bfc1ec5ae3331f66289f3c63e
sarthakeash <f20200060@pilani.bits-pilani.ac.in>
2022-04-06 17:06:35 +0530

    frontend ui completed

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express();
 const port = 6969;
 var mysql = require('mysql');
+const reload = require('livereload');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 
 var con = mysql.createConnection({
@@ -11,22 +14,28 @@ var con = mysql.createConnection({
     database: "mydb"
   });
   
-  con.connect(function(err) {
-    if (err) throw err;
-      console.log("Connected!");
-      var sql = "CREATE TABLE custors (name VARCHAR(255), address VARCHAR(255))";
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Table created");
-      });
-  });
+//   con.connect(function(err) {
+//     if (err) throw err;
+//       console.log("Connected!");
+//       var sql = "CREATE TABLE custors (name VARCHAR(255), address VARCHAR(255))";
+//       con.query(sql, function (err, result) {
+//         if (err) throw err;
+//         console.log("Table created");
+//       });
+//   });
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('home');
 });
-   
+
+app.get('/user/:uid', (req, res) => {
+  var uid= req.params.uid
+     res.render('info')
+   })
 app.listen(port, () => {
   console.log(`Example app listening yes on port ${port}!`)
 });
+
+
