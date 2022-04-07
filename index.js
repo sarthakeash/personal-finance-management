f4dcbb98452ecb1a41970f606a86c1cdb0ba604c
darshil0805 <darshilfcbayern@gmail.com>
2022-04-07 17:25:57 +0530

    sql

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 const port = 6969;
-var mysql = require('mysql');
-const reload = require('livereload');
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-
+var mysql = require("mysql");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "mydb"
+  host: "localhost",
+  user: "root",
+  password: "dtj@sql123",
+  database: "fin_man",
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM users", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
   });
-  
-//   con.connect(function(err) {
-//     if (err) throw err;
-//       console.log("Connected!");
-//       var sql = "CREATE TABLE custors (name VARCHAR(255), address VARCHAR(255))";
-//       con.query(sql, function (err, result) {
-//         if (err) throw err;
-//         console.log("Table created");
-//       });
-//   });
-
-
-
-app.get('/', (req, res) => {
-    res.render('home');
 });
 
-app.get('/user/:uid', (req, res) => {
-  var uid= req.params.uid
-     res.render('info')
-   })
+const users = [
+  { uid: 123456789012, name: "Darshil Chutiya" },
+  { uid: 123456789013, name: "Darshil Harami" },
+  { uid: 123456789014, name: "Darshil Incel" },
+  { uid: 123456789015, name: "Darshil Kutta" },
+];
+
+const transactions=[
+{accNo:1234, nature:"credit", amt:988, bank:"icici"},{accNo:1434, nature:"debit", amt:800, bank:"axis"}
+]
+
+
+const bankAccs=[
+  {accNo:1234, balance:988, bank:"icici"},{accNo:1434, balance:800, bank:"axis"}
+  ]
+
+app.get("/", (req, res) => {
+  res.render("home" ,{users:users});
+});
+
+app.get("/user/:uid", (req, res) => {
+  var uid = req.params.uid;
+  res.render("info");
+});
 app.listen(port, () => {
-  console.log(`Example app listening yes on port ${port}!`)
+  console.log(`Example app listening yes on port ${port}!`);
 });
-
-
