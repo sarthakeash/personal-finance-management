24a7f51919d8d705d520778ac4a2202c84cf2e4a
sarthakeash <f20200060@pilani.bits-pilani.ac.in>
2022-04-12 03:30:06 +0530

    integration done

--- index.js
+++ index.js
@@ -23,22 +23,19 @@ var transactions;
 var netWorth;
 var accounts;
 var owns;
+var weekly_expense;
+var monthly_expense = { week1: "", week2: "", week3: "", week4: "" };
+
 con.connect(function(err) {
 	if (err) throw err
 });
 app.get("/", (req, res) => {
     con.query("SELECT * FROM users", function (err, result, fields) {
       if (err) throw err;
-      console.log(result);
-      users = result
+            users = result
   res.render("home" ,{users:users});
     });
 });
-
- function getUserData(uid, transactions, bankAccs) {
- 
-   
-}
 
  
 
@@ -46,32 +43,62 @@ app.get("/user/:uid", async(req, res) => {
   var uid = req.params.uid;
   con.query(`SELECT name FROM users where aadharNo=${uid}`, function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     name = result
   });
+
+
   con.query(`SELECT * FROM weekly_transactions where aadharNo=${uid}`, function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     transactions = result
   });
+
+
   con.query(`SELECT * FROM accounts where aadharNo=${uid}`,  function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     accounts = result;
   });
-  
+
+
+  con.query(`SELECT * FROM weekly_expense where aadharNo=${uid}`,  function (err, result, fields) {
+    if (err) throw err;
+    weekly_expense = result;
+  });
+
+
   con.query(`SELECT net_worth FROM networth where aadharNo=${uid}`,  function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     netWorth = result;
+  });
+
+  con.query(`select sum(t.amount) as expense from transactions t natural join accounts a where (t.dates > current_date - interval 7 day) and (t.isCredit=0) and (a.aadharNo= ${uid}) order by aadharNo,dates;`,  function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    monthly_expense.week1=result[0].expense;
+  });
+
+  con.query(`select sum(t.amount) as expense from transactions t natural join accounts a where (t.dates > current_date - interval 14 day) and (t.dates < current_date - interval 7 day) and (t.isCredit=0) and (a.aadharNo=${uid}) order by aadharNo,dates;`,  function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    monthly_expense.week2=result[0].expense;
+  });
+  
+  con.query(`select sum(t.amount) as expense from transactions t natural join accounts a where (t.dates > current_date - interval 21 day) and (t.dates < current_date - interval 14 day) and (t.isCredit=0) and (a.aadharNo=${uid}) order by aadharNo,dates;`,  function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    monthly_expense.week3=result[0].expense;
+  });
+
+
+  con.query(`select sum(t.amount) as expense from transactions t natural join accounts a where (t.dates > current_date - interval 28 day) and (t.dates < current_date - interval 21 day) and (t.isCredit=0) and (a.aadharNo=${uid}) order by aadharNo,dates;`,  function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    monthly_expense.week4=result[0].expense;
   });
 
   con.query(`SELECT * FROM owns where aadharNo=${uid}`,  function (err, result, fields) {
     if (err) throw err;
-    console.log(result);
     owns = result;
-    
-  res.render("info", { transactions: transactions, accounts: accounts, owns: owns, netWorth: netWorth , name:name});
+  res.render("info", { transactions: transactions, accounts: accounts, owns: owns, netWorth: netWorth , name:name, weekly_expense:weekly_expense, monthly_expense:monthly_expense});
   });
 });
 
